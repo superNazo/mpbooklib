5edb1174f429efb231397d03aba99e011ded82b9
Nazarii <ivankivnazo@gmail.com>
2018-03-03 10:24:39 +0200

    T-XXX: replace sprockets with webpack - remove dependencies: turbolinks, coffee, jquery, sprockets - remove old configs - webpack configs

--- Gemfile
+++ Gemfile
@@ -10,11 +10,10 @@ ruby '2.5.0'
 gem 'rails', '~> 5.1.4'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.7'
-gem 'sass-rails', '~> 5.0'
-gem 'uglifier', '>= 1.3.0'
 
-gem 'coffee-rails', '~> 4.2'
-gem 'turbolinks', '~> 5'
+gem 'webpacker'
+gem 'foreman'
+
 gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -27,5 +26,13 @@ module Mpbooklib
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
   end
 end
